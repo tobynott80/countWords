1c93e8aa8d246a8487cd26403a2d5914107cb19d
helene de Ribaupierre <deribaupierreh@cardiff.ac.uk>
2021-05-14 14:12:12 +0200

    change in the CSV

--- src/main/java/com/cm6121/countWord/app/utilityFile/FileReader.java
+++ src/main/java/com/cm6121/countWord/app/utilityFile/FileReader.java
@@ -2,8 +2,11 @@ package com.cm6121.countWord.app.utilityFile;
 
 import com.opencsv.CSVReader;
 import com.opencsv.exceptions.CsvException;
+
 import java.io.File;
+import java.io.FileInputStream;
 import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -15,15 +18,32 @@ public class FileReader {
      * @param file, the CSV file
      * @return a List of String array
      */
-    public List<String[]> readCSVMethod1(File file){
-        List<String[]>strings = new ArrayList<>();
+    public List<String[]> readCSVMethod1 (File file) {
+        List<String[]> strings = new ArrayList<>();
         try {
-            CSVReader reader = new CSVReader(new java.io.FileReader(file));
-            reader.skip(1);
-            strings = reader.readAll();
-        }catch(IOException | CsvException e){
+            CSVReader reader = new CSVReader(
+                    new InputStreamReader(new FileInputStream(file), "UTF-8"));
+            {
+                reader.skip(1);
+                strings = reader.readAll();
+            }
+            System.out.println(strings);
+        } catch (IOException | CsvException e) {
             System.out.println(e.getStackTrace());
         }
         return strings;
     }
+
+    /**
+     * This method is used in several tests to return a list of files that is contained in a folder
+     * This method should not be changed.
+     * @param folder String that represent the path of the folder that we want the list of files
+     * @return a array of files
+     */
+    public static File[] getResourceFolderFiles (String folder) {
+        return new File(folder).listFiles();
+    }
+
+
+
 }
